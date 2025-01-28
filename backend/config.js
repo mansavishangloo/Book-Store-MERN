2e0ca50347db45341ffa6861eafa4b3f9e7b7207
Mansavi Shangloo <mansavishangloo999@gmail.com>
2025-01-29 02:09:30 +0530

    get single data api

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export const PORT = 5555;
 
-export const mongoDBURL = `mongodb+srv://shangloomansavi:shangloomansavi@book-store-mern.n2qvr.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN`;
+export const mongoDBURL =
+  "mongodb+srv://shangloomansavi:shangloomansavi@book-store-mern.n2qvr.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN";
