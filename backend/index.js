3770e243ef75258d5015ba706347a6a5e21e0a12
Mansavi Shangloo <mansavishangloo999@gmail.com>
2025-02-02 19:05:47 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,19 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (request, response) => {
   console.log(request);
